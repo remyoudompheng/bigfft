87ff70a4cdd5be145a952d030678c27206b241c0
Corey Mutter <CoreyMutter@eaton.com>
2021-11-17 10:04:19 -0500

    Add FUNCDATA to ASM trampolines so we can use -buildmode=shared.
    Signed-off-by: Corey Mutter <CoreyMutter@eaton.com>

--- arith_386.s
+++ arith_386.s
@@ -4,33 +4,41 @@
 
 // func addVV(z, x, y []Word) (c Word)
 TEXT ·addVV(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addVV(SB)
 	JMP	math∕big·addVV(SB)
 
 // func subVV(z, x, y []Word) (c Word)
 TEXT ·subVV(SB),NOSPLIT,$0
+	FUNCDATA $0, ·subVV(SB)
 	JMP	math∕big·subVV(SB)
 
 // func addVW(z, x []Word, y Word) (c Word)
 TEXT ·addVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addVW(SB)
 	JMP	math∕big·addVW(SB)
 
 // func subVW(z, x []Word, y Word) (c Word)
 TEXT ·subVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·subVW(SB)
 	JMP	math∕big·subVW(SB)
 
 // func shlVU(z, x []Word, s uint) (c Word)
 TEXT ·shlVU(SB),NOSPLIT,$0
+	FUNCDATA $0, ·shlVU(SB)
 	JMP	math∕big·shlVU(SB)
 
 // func shrVU(z, x []Word, s uint) (c Word)
 TEXT ·shrVU(SB),NOSPLIT,$0
+	FUNCDATA $0, ·shrVU(SB)
 	JMP	math∕big·shrVU(SB)
 
 // func mulAddVWW(z, x []Word, y, r Word) (c Word)
 TEXT ·mulAddVWW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·mulAddVWW(SB)
 	JMP	math∕big·mulAddVWW(SB)
 
 // func addMulVVW(z, x []Word, y Word) (c Word)
 TEXT ·addMulVVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addMulVVW(SB)
 	JMP	math∕big·addMulVVW(SB)
 
--- arith_arm.s
+++ arith_arm.s
@@ -4,33 +4,41 @@
 
 // func addVV(z, x, y []Word) (c Word)
 TEXT ·addVV(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addVV(SB)
 	B	math∕big·addVV(SB)
 
 // func subVV(z, x, y []Word) (c Word)
 TEXT ·subVV(SB),NOSPLIT,$0
+	FUNCDATA $0, ·subVV(SB)
 	B	math∕big·subVV(SB)
 
 // func addVW(z, x []Word, y Word) (c Word)
 TEXT ·addVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addVW(SB)
 	B	math∕big·addVW(SB)
 
 // func subVW(z, x []Word, y Word) (c Word)
 TEXT ·subVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·subVW(SB)
 	B	math∕big·subVW(SB)
 
 // func shlVU(z, x []Word, s uint) (c Word)
 TEXT ·shlVU(SB),NOSPLIT,$0
+	FUNCDATA $0, ·shlVU(SB)
 	B	math∕big·shlVU(SB)
 
 // func shrVU(z, x []Word, s uint) (c Word)
 TEXT ·shrVU(SB),NOSPLIT,$0
+	FUNCDATA $0, ·shrVU(SB)
 	B	math∕big·shrVU(SB)
 
 // func mulAddVWW(z, x []Word, y, r Word) (c Word)
 TEXT ·mulAddVWW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·mulAddVWW(SB)
 	B	math∕big·mulAddVWW(SB)
 
 // func addMulVVW(z, x []Word, y Word) (c Word)
 TEXT ·addMulVVW(SB),NOSPLIT,$0
+	FUNCDATA $0, ·addMulVVW(SB)
 	B	math∕big·addMulVVW(SB)
 
